b669de63df5f6c2eae41c2b433f41fdce1c68dad
chenchao <1024204974@qq.com>
2026-01-21 20:37:01 +0800

    chore: Cleanup unused files and directories
    - Deleted redundant _deps directory at root
    - Deleted unused server/src directory (server_config.cpp was duplicate of utils/server_config.cpp)
    - Deleted empty server/net/tcp_connection_inline.cpp
    - Removed stale CTestTestfile.cmake files from source tree

--- tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chenchao/code/cplusplus/chatroom/tests
-# Build directory: /home/chenchao/code/cplusplus/chatroom/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/chenchao/code/cplusplus/chatroom/tests/unit_tests[1]_include.cmake")
-include("/home/chenchao/code/cplusplus/chatroom/tests/integration_tests[1]_include.cmake")
